a57eb991e524db20b0aed95516a0b5d28a3e8c38
Edvilla-50 <eddieseashells@gmail.com>
2026-09-16 00:06:16 -0600

    I forgot www

--- dashboard.php
+++ dashboard.php
@@ -562,9 +562,9 @@ $currentHtml = $data["html"] ?? "<html><body><h1>New Profile</h1></body></html>"
 
   function loadRecentlyPlayed() {
 
-        fetch("https://radius-create.com/music-proxy.php?id=${uid}&t=" + Date.now(), {
+        fetch("https://www.radius-create.com/music-proxy.php?id=" + uid + "&t=" + Date.now(), {
     cache: "no-store"
-    })
+    });
 
       .then(function(r) {
         if (!r.ok) throw new Error("HTTP " + r.status);
